39494c431f749f741c5ea7fc3c2b6bdbf7be2957
Benno Bielmeier <git@bbenno.com>
2022-06-04 23:07:40 +0200

    Refactor tests

--- test/test_languages.rb
+++ test/test_languages.rb
@@ -8,7 +8,7 @@ class TestLanguages < Minitest::Test
   end
 
   ::Languages::TYPES.each do |type|
-    define_method "test_it_provides_languages_of_type_#{type}" do
+    define_method "test_it_provides_scope_for_type_#{type}" do
       languages = ::Languages.public_send(type)
 
       assert_kind_of Enumerable, languages
@@ -17,25 +17,33 @@ class TestLanguages < Minitest::Test
   end
 
   ::Languages::SCOPES.each do |scope|
-    define_method "test_it_provides_languages_of_scope_#{scope}" do
-      methodname = "#{scope}_language" unless scope.end_with? 'language'
-      languages = ::Languages.public_send("#{methodname || scope}s")
+    define_method "test_it_provides_scope_for_scope_#{scope}" do
+      method_name = "#{scope}_language" unless scope.end_with? 'language'
+      languages = ::Languages.public_send("#{method_name || scope}s")
 
       assert_kind_of Enumerable, languages
       assert_instance_of ::Languages::Language, languages.first
     end
   end
 
-  def test_alpha2_has_string_length_2
+  def test_all_alpha2_codes_have_string_length_2
     assert(::Languages.all.map(&:alpha2).compact.all? { |a| a.size == 2 })
   end
 
-  def test_alpha3_has_string_length_2
+  def test_all_alpha3_codes_have_string_length_3
     assert(::Languages.all.map(&:alpha3).compact.all? { |a| a.size == 3 })
   end
 
+  def test_all_languages_have_a_type
+    refute(::Languages.all.map(&:type).any?(&:nil?))
+  end
+
+  def test_all_languages_have_a_scope
+    refute(::Languages.all.map(&:scope).any?(&:nil?))
+  end
+
   %i[iso639_1 iso639_2b iso639_2t iso639_3 name].each do |attr|
-    define_method "test_#{attr}_is_unique" do
+    define_method "test_attribute_#{attr}_is_unique" do
       assert_nil(::Languages.all.map(&:"#{attr}").compact.tally.detect { |_, c| c > 1 })
     end
   end
